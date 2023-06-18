e28cc6a4c359336fdd43dbd958552b4883bd8cee
Harry <harryedgar@hotmail.co.uk>
2023-06-18 10:16:07 +0100

    add more robust email and postcode validation. Add submit button

--- src/index.js
+++ src/index.js
@@ -1,31 +1,44 @@
 const modal = document.querySelector('.modal')
 const modalBtn = document.querySelector('.modal-button')
 const closemodalBtn = document.querySelector('.close-modal-button')
-const password = document.getElementById('password')
-const passwordConfirm = document.getElementById('confirm')
+const passwordInput = document.getElementById('password')
+const passwordConfInput = document.getElementById('confirm')
 const validationContainer = document.querySelector('.pass-validation-container')
-
-// to-do - eye to view passwords
-//       - more robust email regex
-//       - form submission
+const emailInput = document.getElementById('email')
+const postcodeInput = document.getElementById('postcode')
+const submitBtn = document.querySelector('.submit-button')
 
 modalBtn.addEventListener('click', () => {modal.showModal()})
 closemodalBtn.addEventListener('click', () => {modal.close()})
 
-password.addEventListener('focus', () => {
+emailInput.addEventListener('input', () => {
+    const isValid = /^[^@]+@[^@.]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?$/
+    .test(emailInput.value);
+    emailInput.setCustomValidity(isValid ? '' : 'invalid')
+})
+
+postcodeInput.addEventListener('blur', addSpaceToPostcode)
+
+function addSpaceToPostcode() {
+    const postcode = postcodeInput.value.trim();
+    const updatedPostcode = postcode.replace(/^(.*[A-Za-z]{1,2}[0-9Rr][0-9A-Za-z]?)\s*([0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2})?$/, '$1 $2');
+    postcodeInput.value = updatedPostcode;
+}
+
+passwordInput.addEventListener('focus', () => {
     validationContainer.classList.remove('hidden')
 })
 
-password.addEventListener('focusout', () => {
+passwordInput.addEventListener('blur', () => {
     validationContainer.classList.add('hidden')
 })
 
-password.addEventListener('input', () => {
+passwordInput.addEventListener('input', () => {
     validatePassword()
     checkPasswordsMatch()
 })
 
-passwordConfirm.addEventListener('input', () => {
+passwordConfInput.addEventListener('input', () => {
     checkPasswordsMatch()
 })
 
@@ -34,9 +47,9 @@ function validatePassword() {
     const upperContainer = document.getElementById('validate-upper')
     const specialContainer = document.getElementById('validate-special')
 
-    let containsLower = /[a-z]/.test(password.value)
-    let containsUpper = /[A-Z]/.test(password.value)
-    let containsSpecial = /[0-9`¬|!"£$€%^&*()\-_=+{}[\];:'@#~\\,<>./?]/.test(password.value)
+    let containsLower = /[a-z]/.test(passwordInput.value)
+    let containsUpper = /[A-Z]/.test(passwordInput.value)
+    let containsSpecial = /[0-9`¬|!"£$€%^&*()\-_=+{}[\];:'@#~\\,<>./?]/.test(passwordInput.value)
 
     containsLower === true ? setPasswordHint(lowerContainer, "check", "add") : setPasswordHint(lowerContainer, "cross", "remove")
     containsUpper === true ? setPasswordHint(upperContainer, "check", "add") : setPasswordHint(upperContainer, "cross", "remove")
@@ -49,10 +62,10 @@ function setPasswordHint(container, status, classStatus) {
 }
 
 function checkPasswordsMatch() {
-    if (password.value != passwordConfirm.value) {
-        passwordConfirm.setCustomValidity("invalid")
+    if (passwordInput.value != passwordConfInput.value) {
+        passwordConfInput.setCustomValidity("invalid")
     }
-    else if (password.value === passwordConfirm.value) {
-        passwordConfirm.setCustomValidity("")
+    else if (passwordInput.value === passwordConfInput.value) {
+        passwordConfInput.setCustomValidity("")
     }
 }
